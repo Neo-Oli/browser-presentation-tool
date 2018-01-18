6a812781684c35094005db5886f71110fe9436c0
Oliver Schmidhauser <oli@glow.li>
2018-01-18 09:53:37 +0100

    Make template.php work when running on a webserver

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-index.html: slides.php styles.css config.php template/presentation.js template/system.css template/template.php
-	php -d include_path=$(pwd) ./template/template.php > index.html
+index.html: slides.php styles.css config.php template/presentation.js template/system.css template/template.php Makefile
+	cd template;\
+	php ./template.php > ../index.html
 clean:
 	rm -f index.html
 
--- template/template.php
+++ template/template.php
@@ -1,20 +1,19 @@
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
-            <?php include("config.php");?>
+            <?php include("../config.php");?>
             <title><?=$title?></title>
             <style>
                 <?php include("system.css");?>
             </style>
             <style>
-                <?php include("styles.css");?>
+                <?php include("../styles.css");?>
             </style>
     </head>
     <body>
-        <?php include("slides.php");?>
+        <?php include("../slides.php");?>
         <a class="prev" href="javascript:prev()"></a>
         <a class="next" href="javascript:next()"></a>
         <script>
