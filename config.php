0be36710b6f985034790a34280f87495af8ec2e9
Oliver Schmidhauser <oli@glow.li>
2018-11-07 01:26:19 +0100

    prepare example for glow.li

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 $title="Browser Presentation Tool - Demo Presentation";
-$author="Oliver Schmidhauser";
+$author="Glow";
 $date="18.1.2018";
 ?>
 <style>
     :root{
-        --footerHeight:70px;
+        --footerHeight:100px;
     }
 </style>
+
